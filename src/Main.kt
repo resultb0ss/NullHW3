4afba7a7810c76f4cf787bf3d5491fd250e37002
Admin <reteamboss@gmail.com>
2024-09-09 15:28:37 +0300

    3

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    println("Hello World!")
+    val array = arrayOf("Привет","как", null,"дела")
+
+    var newArray: List<String?> = array.filter { it != null }
+    println(array.contentToString())
+    println(newArray)
+
+
 }
